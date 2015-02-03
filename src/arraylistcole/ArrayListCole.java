8fa47d45ecfec2890fd01495838db92b1e46f40e
Layun <Layun@Layun-PC>
2015-02-03 21:52:20 +0100

    http://jagonzalez.org/arraylist-de-objetos-en-java-con-ejemplo-practico/# Surrender

--- src/arraylistcole/ArrayListCole.java
+++ src/arraylistcole/ArrayListCole.java
@@ -1,27 +1,12 @@
-
 package arraylistcole;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 public class ArrayListCole {
 
     public static void main(String[] args) {
-        Cole obx=new Cole();
-   int op;
-        do{
-        op = Cole.menu();
-    switch(op){
-            case 1:crearArray();
-            break;
-            case 2:ver();
-            break;
-            case 3:engadir();
-            break;
-            case 4:borrar();
-            break;
-            case 5:ordenar();
-            break;
-            case 6:System.exit(0);
-    }
-        }while(op!=6);
-    }
-    
+        Cole obx = new Cole();
+        obx.opcions();
+}
 }
--- src/arraylistcole/Cole.java
+++ src/arraylistcole/Cole.java
@@ -1,15 +1,15 @@
-
 package arraylistcole;
 
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
-
 public class Cole {
-private String nome;
-private int nota;
 
-ArrayList<Cole>alum=new ArrayList<Cole>();
+    private String nome;
+    private int nota;
+
+    ArrayList<Cole> alum = new ArrayList<Cole>();
+    
     public Cole(String nome, int nota) {
         this.nome = nome;
         this.nota = nota;
@@ -38,10 +38,69 @@ ArrayList<Cole>alum=new ArrayList<Cole>();
     public String toString() {
         return "Cole{" + "nome=" + nome + ", nota=" + nota + '}';
     }
-    public static int menu(){
-    int opcion=Integer.parseInt(JOptionPane.showInputDialog("****MENU***\n"+"1->Crear Array\n"+"2->Amosar\n"+"3->engadir elemento\n"+"4->borrar elemento\n"+"5->Ordear\n"+"6->Sair do programa"));
+
+    public int menuSelect() {
+        int opcion = JOptionPane.showOptionDialog(null, "Selecciona unha opción", "Menu", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{"Crear Array", "Amosar", "engadir elemento", "borrar elemento", "Ordear", "Sair do programa"}, "Sair do programa");
+        return opcion + 1;
+
+    }
+
+    public void crearArray() {
+        
+
+    }
+
     
-    return opcion;
-}
-    
-}
+
+    public void opcions() {
+       
+         int op;
+        do {
+
+            op = this.menuSelect();
+            switch (op) {
+                case 1:
+                    if (op == 1) {
+                    int na;
+                    String ne;
+                    nome = JOptionPane.showInputDialog(null, "Escribe o nome: ");
+                    nota = Integer.parseInt(JOptionPane.showInputDialog(null, "Escribe a nota: "));
+                    alum.add(new Cole(nome, nota));
+                        
+                    }
+                    break;
+                case 2:
+                    if (op == 2) {
+                    
+                       for( int i = 0 ; i  < alum.size(); i++)
+                            System.out.println(alum.toString());
+                    }
+                    break;
+                case 3:
+                    if (op == 3) {
+                        System.out.println("engadir");//this.engadir();
+                    }
+                    break;
+                case 4:
+                    if (op == 4) {
+                        System.out.println("borrar");//this.borrar();
+                    }
+                    break;
+                case 5:
+                    if (op == 5) {
+                        System.out.println("ordenar");//this.ordenar();
+                    }
+                    break;
+                case 6:
+                    if (op == 6) {
+                        System.exit(0);
+                    }
+            }
+        } while (op != 6 && op == -1);
+        
+
+    }
+    }
+
+  
+
