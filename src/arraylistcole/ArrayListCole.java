d50c7e501782fcabff8fd6a825072f86fa181ef4
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2015-02-04 11:43:44 +0100

    falta borrar ordenar , 'x' ventana pa cerrar

--- src/arraylistcole/ArrayListCole.java
+++ src/arraylistcole/ArrayListCole.java
@@ -7,6 +7,42 @@ public class ArrayListCole {
 
     public static void main(String[] args) {
         Cole obx = new Cole();
-        obx.opcions();
-}
+        int op;
+        do {
+
+            op = Cole.menuSelect();
+            switch (op) {
+                case 1:
+                    if (op == 1) {
+                        obx.crearArray();
+                    }
+                    break;
+                case 2:
+                    if (op == 2) {
+                        obx.amosar();
+                    }
+                    break;
+                case 3:
+                    if (op == 3) {
+
+                        obx.engadir();
+                    }
+                    break;
+                case 4:
+                    if (op == 4) {
+                        obx.borrar();
+                    }
+                    break;
+                case 5:
+                    if (op == 5) {
+                        obx.ordear();
+                    }
+                    break;
+                case 6:
+                    if (op == 6 || op == 0) {
+                        System.exit(0);
+                    }
+            }
+        } while (op != 6 || op != 0);
+    }
 }
--- src/arraylistcole/Cole.java
+++ src/arraylistcole/Cole.java
@@ -1,6 +1,7 @@
 package arraylistcole;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import javax.swing.JOptionPane;
 
 public class Cole {
@@ -9,7 +10,7 @@ public class Cole {
     private int nota;
 
     ArrayList<Cole> alum = new ArrayList<Cole>();
-    
+
     public Cole(String nome, int nota) {
         this.nome = nome;
         this.nota = nota;
@@ -39,68 +40,60 @@ public class Cole {
         return "Cole{" + "nome=" + nome + ", nota=" + nota + '}';
     }
 
-    public int menuSelect() {
+    public static int menuSelect() {
         int opcion = JOptionPane.showOptionDialog(null, "Selecciona unha opción", "Menu", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{"Crear Array", "Amosar", "engadir elemento", "borrar elemento", "Ordear", "Sair do programa"}, "Sair do programa");
         return opcion + 1;
 
     }
 
+    public String pedirNome() {
+        String nom = JOptionPane.showInputDialog("introduce nome");
+        return nom;
+    }
+
+    public int pedirNota() {
+        int nom = Integer.parseInt(JOptionPane.showInputDialog("introduce nota"));
+        return nom;
+    }
+
     public void crearArray() {
-        
+        String auxNome;
+        int auxNota;
+        auxNome = this.pedirNome();
+        while (auxNome.compareToIgnoreCase("fin") != 0) {
+            auxNota = this.pedirNota();
+            alum.add(new Cole(auxNome, auxNota));
+            auxNome = this.pedirNome();
+        }
+    }
+
+    public void amosar() {
+        for (Cole al : alum) {
+            JOptionPane.showMessageDialog(null, al.toString());
+        }
+    }
+
+    public void engadir() {
+        String auxNome;
+        int auxNota;
+        int pos = Integer.parseInt(JOptionPane.showInputDialog("introduce a posición na que queres engadir"));
+        auxNome = this.pedirNome();
+
+        auxNota = this.pedirNota();
+        alum.add(pos, new Cole(auxNome, auxNota));
 
     }
 
-    
-
-    public void opcions() {
-       
-         int op;
-        do {
-
-            op = this.menuSelect();
-            switch (op) {
-                case 1:
-                    if (op == 1) {
-                    int na;
-                    String ne;
-                    nome = JOptionPane.showInputDialog(null, "Escribe o nome: ");
-                    nota = Integer.parseInt(JOptionPane.showInputDialog(null, "Escribe a nota: "));
-                    alum.add(new Cole(nome, nota));
-                        
-                    }
-                    break;
-                case 2:
-                    if (op == 2) {
-                    
-                       for( int i = 0 ; i  < alum.size(); i++)
-                            System.out.println(alum.toString());
-                    }
-                    break;
-                case 3:
-                    if (op == 3) {
-                        System.out.println("engadir");//this.engadir();
-                    }
-                    break;
-                case 4:
-                    if (op == 4) {
-                        System.out.println("borrar");//this.borrar();
-                    }
-                    break;
-                case 5:
-                    if (op == 5) {
-                        System.out.println("ordenar");//this.ordenar();
-                    }
-                    break;
-                case 6:
-                    if (op == 6) {
-                        System.exit(0);
-                    }
-            }
-        } while (op != 6 && op == -1);
-        
-
-    }
+    public void borrar() {
+        String nomClean = JOptionPane.showInputDialog("introduce nome a borrar");
+        /**
+         * for (Cole al : alum) { if (al.nome.compareToIgnoreCase(nomClean) ==
+         * 0);
+        alum.*
+         */
     }
 
-  
-
+    public void ordear() {
+        Collections.sort(alum, null);
+    }
+}
